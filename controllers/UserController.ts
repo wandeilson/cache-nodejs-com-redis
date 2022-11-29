439642a029070e1abb413256f7448b2c274ab1d0
Wandeilson Gomes da Silva <wandeilsongomes@gmail.com>
2022-11-29 08:21:46 -0300

    Docker-compose e Dockerfile funcional

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -1,16 +1,25 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express'; 
+import redis from '../src/lib/cache';
 
 const prisma = new PrismaClient();
 
 class UserController {
     static async find (req: Request, res: Response){
         try{
-            console.time("Find users");
-            
-            const users = await prisma.user.findMany();
+            const cacheKey = "users:all";
 
+            const cachedUsers = await redis.get(cacheKey);
+            
+            if(cachedUsers){
+                return res.json(JSON.parse(cachedUsers));
+            }
+
+            console.time("Find users");
+            const users = await prisma.user.findMany();
             console.timeEnd("Find users")
+            
+            await redis.set(cacheKey, JSON.stringify(users))
             return res.json(users);
         } catch( e ){
             console.log(e);
